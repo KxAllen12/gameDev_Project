e2bf83d9315c8102d2f064253793a1fe64858622
KxAllen12 <Allan.perez01@utrgv.edu>
2025-03-14 13:33:52 -0500

    Initial commit of Unity project

---
+++ 2D Top Down RPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+
+    private PlayerControls playerControls;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        AdjustPlayerFacingDirection();
+    }
+
+    private void PlayerInput()
+    {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
+
+        // Add dead zone to avoid tiny inputs being registered
+        if (movement.magnitude < 0.1f)
+        {
+            movement = Vector2.zero;
+        }
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.deltaTime));
+    }
+
+    public void AdjustPlayerFacingDirection()
+    {
+        // No flipping of sprite when moving up or down, only left or right
+        if (movement.x < 0)
+        {
+            mySpriteRenderer.flipX = true;  // Flip to the left when moving left
+        }
+        else if (movement.x > 0)
+        {
+            mySpriteRenderer.flipX = false;  // Face right when moving right
+        }
+    }
+}
